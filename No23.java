ec7c4ae235ca200ca3d703bc155fd44cfbf514bd
jackBurghardt <112026991+jackBurghardt@users.noreply.github.com>
2022-10-27 10:29:24 -0700

    Add files via upload

---
+++ No23.java
@@ -0,0 +1,12 @@
+public class No23 {
+	public boolean no23(int[] nums) {
+		 boolean d = false;
+		  for (int i = 0; i< 2; i ++) {
+		    if (nums[i]== 2 || nums[i] ==3){
+		      return false;
+		    }
+		    
+		  }
+		  return true;
+		}
+}
